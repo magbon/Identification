0a44557b38a30ac84fcf15baff7b8e18cd8bc8a6
Magda Bonczkowska <magda1231@gmail.com>
2018-01-02 22:26:32 +0100

    wieksza i dodatnia

---
+++ zadanie1.cbl
@@ -0,0 +1,18 @@
+﻿IDENTIFICATION DIVISION.
+PROGRAM-ID. HELLO-WORLD.
+DATA DIVISION.
+    WORKING-STORAGE SECTION.
+        77 X PIC 99.
+PROCEDURE DIVISION.
+    SET X TO 3.
+
+    IF X > 5 
+        DISPLAY "Liczba jest wieksza niż 5"
+    ELSE 
+        IF X = 5
+            DISPLAY "Liczba 5"
+        ELSE
+            DISPLAY  "Liczba jest mniejsza niż 5"
+        END-IF
+    END-IF.
+STOP RUN.
---
+++ zadanie2.cbl
@@ -0,0 +1,19 @@
+﻿	IDENTIFICATION DIVISION.
+	PROGRAM-ID. HELLO-WORLD.
+	DATA DIVISION.
+    	WORKING-STORAGE SECTION.
+        	77 X PIC S9(3).                
+* S -> SIGN
+	PROCEDURE DIVISION.
+    	SET X TO -3.
+
+    	IF X > 0 
+        	DISPLAY "Liczba jest dodatnia"
+    	ELSE 
+        	IF X = 0
+            	DISPLAY "Liczba 0"
+            ELSE
+				DISPLAY  "Liczba jest ujemna"
+            END-IF
+    	END-IF.
+	STOP RUN.
