b49b199ecec88a5d71dc896e06a4ea13384b8f98
Magda Bonczkowska <magda1231@gmail.com>
2018-01-29 21:37:12 +0100

    for

---
+++ cookbook/likeFor.cbl
@@ -0,0 +1,28 @@
+﻿	IDENTIFICATION DIVISION.
+	PROGRAM-ID. IDEONE.
+
+	ENVIRONMENT DIVISION.
+
+	DATA DIVISION.
+		WORKING-STORAGE SECTION.
+		01	LIKE-I PIC 9(3).
+		
+	PROCEDURE DIVISION.
+	*>	your code goes here
+	
+		
+			PERFORM TRY-FOR
+			VARYING LIKE-I FROM 0 BY 10  
+			UNTIL LIKE-I > 100.
+      *                       FOR I = 0 <- FROM 0 
+      *		      I = I + 10 <- BY 10
+      *		      I < 100 <- UNTIL I > 100		
+					
+				STOP-NOW.
+				STOP RUN.
+					
+			TRY-FOR.
+			DISPLAY LIKE-I.
+			
+						
+					
